5c94a3f1a19683b02b6a4d84e2c2137986b80b73
Aranyan Rajesh <aranyan.rajesh@gmail.com>
2023-12-16 20:27:40 -0800

    Finished the lists python excercise and added the code here.

---
+++ pythonExercises/Lists.py
@@ -0,0 +1,13 @@
+# Defining the right list parameters
+numbers = [1,2,3]
+strings = ["hello","world"]
+names = ["John", "Eric", "Jessica"]
+
+# Setting the second name parameter to the second name on the names list (Eric)
+second_name = names[1]
+
+
+# This code should write out the filled lists!
+print(numbers)
+print(strings)
+print("The second name on the names list is %s" % second_name)
