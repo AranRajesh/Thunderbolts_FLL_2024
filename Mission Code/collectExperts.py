e579f75e881720b40919a2f2ba93a078d8c16648
Aranyan Rajesh <aranyan.rajesh@gmail.com>
2024-01-05 19:24:43 -0800

    Created code to collect experts, it's working, but we are not collecting Noah the Sound Engineer.

---
+++ Mission Code/collectExperts.py
@@ -0,0 +1,45 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, Light, ColorLightMatrix
+from pybricks.parameters import Port, Direction, Color, Stop
+from pybricks.geometry import Axis
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch
+from pybricks.pupdevices import ColorSensor
+hub = PrimeHub()
+# Initialize the timer
+timer = StopWatch()
+timer.reset()
+
+
+# Initialize both motors. In this example, the motor on the
+# left must turn counterclockwise to make the robot go forward.
+left_motor = Motor(Port.A, Direction.COUNTERCLOCKWISE)
+right_motor = Motor(Port.B)
+# Initialize the color sensors
+
+right_sensor = ColorSensor(Port.F)
+left_sensor = ColorSensor(Port.E)
+
+# Initialize the arm motors
+right_arm_motor = Motor(Port.C)
+left_arm_motor = Motor(Port.D)
+
+# Initialize the drive base. In this example, the wheel diameter is 56mm.
+# The distance between the two wheel-ground contact points is 112mm.
+bot = DriveBase(left_motor, right_motor, wheel_diameter=56, axle_track=80)
+
+
+print (bot.settings())
+bot.settings(300, 300, 233, 300)
+
+# Optionally, uncomment the line below to use the gyro for improved accuracy.
+bot.use_gyro(True)
+print('Initialized Bot')
+
+bot.straight(150)
+bot.turn(-45)
+bot.straight(160)
+bot.turn(-60)
+bot.straight(200)
+bot.turn(15)
+bot.straight(4000)
