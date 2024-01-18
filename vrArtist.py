4c8158373f17d48a3c1ef181e6eb8b0316a79ad2
AranRajesh <aranyan.rajesh@outlook.com>
2024-01-17 21:11:02 -0800

    Changed chicken to work with the new class system. WORKING

--- vrArtist.py
+++ vrArtist.py
@@ -1,60 +1,33 @@
-from pybricks.hubs import PrimeHub
-from pybricks.pupdevices import Motor, Light, ColorLightMatrix
-from pybricks.parameters import Port, Direction, Color, Stop
-from pybricks.geometry import Axis
-from pybricks.robotics import DriveBase
+from primeBot import primeBot
 from pybricks.tools import wait, StopWatch
-from pybricks.pupdevices import ColorSensor
-hub = PrimeHub()
-# Initialize the timer
-timer = StopWatch()
-timer.reset()
 
+###############################################
+## Write your code in this function 
+## Note you can rename this if you want 
+#----------------------------------------------
 
-# Initialize both motors. In this example, the motor on the
-# left must turn counterclockwise to make the robot go forward.
-left_motor = Motor(Port.A, Direction.COUNTERCLOCKWISE)
-right_motor = Motor(Port.B)
-# Initialize the color sensors
-
-right_sensor = ColorSensor(Port.F)
-left_sensor = ColorSensor(Port.E)
-
-# Initialize the arm motors
-right_arm_motor = Motor(Port.D)
-left_arm_motor = Motor(Port.C)
-
-# Initialize the drive base. In this example, the wheel diameter is 56mm.
-# The distance between the two wheel-ground contact points is 112mm.
-bot = DriveBase(left_motor, right_motor, wheel_diameter=56, axle_track=80)
-
-
-print (bot.settings())
-bot.settings(300, 300, 100, 100)
-# Drive Until Stalled function:
-def drive_straight_until_stalled(speed = 600):
-    bot.drive(speed,0)
-    while (not bot.stalled()):
-         wait(100)
+def vrArtist(bot: primeBot):
+    bot.straight(150)
+    bot.turn(-45)
+    bot.drive(400, 0)
+    wait(2000)
     bot.stop()
+    bot.left_arm_motor.run_angle(5000,-3700)
+    bot.right_arm_motor.run_angle(500, -550)
+    bot.drive(-700,5)
+    wait(2000)
+    bot.stop()
+    # For the code to work, the alignment has to be 20 from right home area!
+    return
 
 
-# Optionally, uncomment the line below to use the gyro for improved accuracy.
-bot.use_gyro(True)
-print('Initialized Bot')
-bot.straight(150)
-bot.turn(-45)
-bot.drive(400, 0)
-wait(2000)
-bot.stop()
-left_arm_motor.run_angle(5000,-3700)
-right_arm_motor.run_angle(500, -550)
-bot.drive(-700,5)
-wait(2000)
-bot.stop()
+if __name__ == "__main__": 
+    missionFunction = vrArtist
+# ----------------------------------------------
+###############################################
+## You don't need to modify anything  below this line
 
-print(timer.time()/1000)
-
-
-# For the code to work, the alignment has to be 20 from right home area!
-# It's completed! Note: This was coded on the second table!
+    timer = StopWatch()
+    myBot = primeBot()
+    missionFunction(myBot)
+    print(f"Completed in {timer.time()/1000} s")
